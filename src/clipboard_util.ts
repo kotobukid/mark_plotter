045854e7c8d3cefc72f1a260f119a00ccf35a546
kotobukid <kotobuki@daqua.jp>
2024-05-17 18:09:51 +0900

    二重resolveを修正

--- src/clipboard_util.ts
+++ src/clipboard_util.ts
@@ -15,6 +15,7 @@ const getClipboardImage = async (): Promise<Blob | null> => {
 
 const blobToDataURL = (blob: Blob): Promise<string> => {
     return new Promise((resolve, reject) => {
+        debugger
         const reader = new FileReader();
         reader.onloadend = () => resolve(reader.result as string);
         reader.onerror = reject;
@@ -22,35 +23,12 @@ const blobToDataURL = (blob: Blob): Promise<string> => {
     });
 }
 
-const prepareImage = (dataUrl: string) => {
-    const svg = document.querySelector('svg');
-    if (svg) {
-        getDataUrlDimensions(dataUrl).then(dimensions => {
-            console.log(dimensions);
-            const img = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-            console.log({
-                width: img.width,
-                height: img.height,
-            })
-            img.setAttributeNS(null, 'href', dataUrl);
-            img.setAttributeNS(null, 'x', '0');
-            img.setAttributeNS(null, 'y', '0');
-            img.setAttributeNS(null, 'height', dimensions.height); // 必要に応じて調整
-            img.setAttributeNS(null, 'width', dimensions.width);  // 必要に応じて調整
-            svg.appendChild(img);
-        }).catch(error => {
-            console.error('Error while getting dimensions from Data URL:', error);
-        });
-    } else {
-        console.error('SVG element not found');
-    }
-}
-
 const getDataUrlDimensions = (dataUrl: string): Promise<{ width: number, height: number }> => {
     return new Promise((resolve, reject) => {
+        debugger
         const img = new Image();
         img.onload = () => {
-            resolve({ width: img.width, height: img.height });
+            resolve({width: img.width, height: img.height});
         };
         img.onerror = reject;
         img.src = dataUrl;
@@ -59,18 +37,17 @@ const getDataUrlDimensions = (dataUrl: string): Promise<{ width: number, height:
 
 const getDataUrlAndDimensions = (dataUrl: string): Promise<{ dataUrl: string, width: number, height: number }> => {
     return new Promise((resolve, reject) => {
+        debugger
         const img = new Image();
         img.onload = () => {
-            resolve({ width: img.width, height: img.height });
+            resolve({
+                dataUrl: dataUrl,
+                width: img.width,
+                height: img.height
+            });
         };
         img.onerror = reject;
         img.src = dataUrl;
-
-        resolve({
-            dataUrl: dataUrl,
-            width: img.width,
-            height: img.height
-        });
     });
 };
 
