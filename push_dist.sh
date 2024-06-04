16f2af9b4eae5333a66f301fd84d9b5fe90d5055
kotobukid <kotobuki@daqua.jp>
2024-06-04 18:05:26 +0900

    デプロイ対象にディレクトリがあると失敗扱いになる

--- push_dist.sh
+++ push_dist.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ssh kotobuki@saturn "mkdir -p /home/kotobuki/svg_marker2/dist"
-scp dist/* kotobuki@saturn:/home/kotobuki/svg_marker/dist
+scp -r dist/* kotobuki@saturn:/home/kotobuki/svg_marker/dist
