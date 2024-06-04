abc27798cf8a8ecbe04f2c9564c6504e459ef2ff
kotobukid <kotobuki@daqua.jp>
2024-06-04 21:18:54 +0900

    ssh接続終了時にプロセスが終了するのでnohupを使う

--- run_container_sample.sh
+++ run_container_sample.sh
@@ -1,8 +1,9 @@
-podman run --rm -it --name my_httpd -p 8081:80 \
+#!/bin/sh
+
+nohup podman run --rm -d --name svg_marker \
 -p 8443:443 \
 -v ~/svg_marker/dist:/usr/local/apache2/htdocs \
 -v ~/svg_marker/certs/server.crt:/usr/local/apache2/conf/server.crt:Z \
--v ~/svg_marker/certs/server.key:/usr/local/apache2/conf/server.key \
--v ~/svg_marker/httpd.conf:/usr/local/apache2/conf/httpd.conf \
-httpd
-
+-v ~/svg_marker/certs/server.key:/usr/local/apache2/conf/server.key:Z \
+-v ~/svg_marker/httpd.conf:/usr/local/apache2/conf/httpd.conf:Z \
+httpd > out.log 2>&1 &
