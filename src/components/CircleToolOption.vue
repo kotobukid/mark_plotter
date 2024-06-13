13c8ecc84e0d6501af86da3943c2b61d0efc168a
kotobukid <kotobuki@daqua.jp>
2024-06-13 21:12:38 +0900

    オプションに説明を付与

--- src/components/CircleToolOption.vue
+++ src/components/CircleToolOption.vue
@@ -13,8 +13,12 @@ const set_option = (option: 'draw_full' | 'draw_half') => {
 
 <template lang="pug">
   div(v-if="tool_store.current === 'circle'")
-    img.tool_option(src="/draw_circle_full.svg" @click="set_option('draw_full')" :data-active="tool_store.circle_tool_option === 'draw_full'")
-    img.tool_option(src="/draw_circle_half.svg" @click="set_option('draw_half')" :data-active="tool_store.circle_tool_option === 'draw_half'")
+    img.tool_option(src="/draw_circle_full.svg" @click="set_option('draw_full')" :data-active="tool_store.circle_tool_option === 'draw_full'"
+      title="矩形から作成"
+    )
+    img.tool_option(src="/draw_circle_half.svg" @click="set_option('draw_half')" :data-active="tool_store.circle_tool_option === 'draw_half'"
+      title="中心から作成"
+    )
 </template>
 
 <style scoped lang="less">
