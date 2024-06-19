484c6b368f06090af7f361afb6c4b3b7d4c50a80
kotobukid <kotobuki@daqua.jp>
2024-06-19 14:32:59 +0900

    LAN内デプロイのパスを変更

--- push_dist.sh
+++ push_dist.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ssh kotobuki@saturn "mkdir -p /home/kotobuki/svg_marker/dist"
-scp -r dist/* kotobuki@saturn:/home/kotobuki/svg_marker/dist
+scp -r dist/* kotobuki@saturn:/home/kotobuki/svg_marker/dist/mark_plotter
